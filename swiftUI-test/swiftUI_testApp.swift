f4a8acbeedc045063beb63797d490fa8d2f3935c
Blackraven93 <reblackraven@gmail.com>
2022-04-05 00:13:58 +0900

    Initial Commit

---
+++ swiftUI-test/swiftUI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_testApp.swift
+//  swiftUI-test
+//
+//  Created by apple developer academy on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
