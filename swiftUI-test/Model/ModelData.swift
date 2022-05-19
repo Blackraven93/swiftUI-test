2bf06fa77f43bd8023317b495e5ab6ada0c64701
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-20 03:31:20 +0900

    Tutorial: Composing Complex Interfaces

--- swiftUI-test/Model/ModelData.swift
+++ swiftUI-test/Model/ModelData.swift
@@ -16,6 +16,17 @@ final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     // model에서 struct를 구성 핸들링 할 수 있는 json을 가져온다.
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+       landmarks.filter { $0.isFeatured }
+   }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
